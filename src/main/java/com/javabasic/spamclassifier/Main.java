211da082f0fef9777b5f92dccea3f2dad0aa3a4f
RITika2522 <ritikachandak2502@gmail.com>
2025-09-30 13:22:54 +0530

    testing done

--- src/main/java/com/javabasic/spamclassifier/Main.java
+++ src/main/java/com/javabasic/spamclassifier/Main.java
@@ -1,38 +1,46 @@
 package com.javabasic.spamclassifier;
-
 import weka.classifiers.bayes.NaiveBayes;
-import weka.classifiers.trees.RandomForest;
-import weka.classifiers.functions.SMO;
-import weka.classifiers.Classifier;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            // Create classifier object
             SpamClassifier classifier = new SpamClassifier();
 
-            // Load ARFF dataset
+            // 1. Load dataset
             classifier.loadDataset("data/spambase.arff");
 
-            // Run Naive Bayes
-            classifier.trainAndEvaluate(new NaiveBayes(), "Naive Bayes");
+            // 2. Train + evaluate Naive Bayes
+            NaiveBayes nb = new NaiveBayes();
+            classifier.trainAndEvaluate(nb, "Naive Bayes");
 
-            // Run Random Forest
-            classifier.trainAndEvaluate(new RandomForest(), "Random Forest");
+            // 3. Save trained model
+            classifier.saveModel(nb, "spam.model");
 
-            // Run SVM (SMO in Weka)
-            classifier.trainAndEvaluate(new SMO(), "SVM (SMO)");
+            // 4. Load model back
+            classifier.loadModel("spam.model");
+            classifier.setDatasetHeader(classifier.getDataset()); // needed for new instances
 
-            // === Save the best model (example: RandomForest) ===
-            String modelPath = "models/spam_randomforest.model";
-            classifier.saveModel(new RandomForest(), modelPath);
+            // 5. CLI input
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\n=== Spam Email Classifier CLI ===");
+            System.out.println("Paste a CSV row (without class label), or type 'exit' to quit.");
 
-            // === Load model back ===
-            Classifier loadedModel = classifier.loadModel(modelPath);
+            while (true) {
+                System.out.print("\nEnter CSV row: ");
+                String input = scanner.nextLine();
 
-            if (loadedModel != null) {
-                System.out.println("Successfully reloaded trained model: " + loadedModel.getClass().getSimpleName());
+                if (input.equalsIgnoreCase("exit")) {
+                    System.out.println("Exiting...");
+                    break;
+                }
+
+                // Classify input row
+                String result = classifier.classifyEmailFromCSV(input);
+                System.out.println("Prediction: " + result);
             }
+
+            scanner.close();
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com/javabasic/spamclassifier/SpamClassifier.java
+++ src/main/java/com/javabasic/spamclassifier/SpamClassifier.java
@@ -1,5 +1,7 @@
 package com.javabasic.spamclassifier;
 
+import weka.core.DenseInstance;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.Classifier;
@@ -10,6 +12,7 @@ import java.util.Random;
 
 public class SpamClassifier {
     private Instances dataset;
+    private Classifier model;
 
     // Load dataset from ARFF
     public void loadDataset(String filepath) {
@@ -58,15 +61,76 @@ public class SpamClassifier {
         }
     }
 
-    // Load model from file
-    public Classifier loadModel(String modelPath) {
+    // Load trained model from file
+    public void loadModel(String modelPath) {
         try {
-            Classifier cls = (Classifier) SerializationHelper.read(modelPath);
-            System.out.println("Model loaded from: " + modelPath);
-            return cls;
+            model = (Classifier) SerializationHelper.read(modelPath);
+            System.out.println("Model loaded from " + modelPath);
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
+        }
+    }
+
+    // Load ARFF header (to create new instances later)
+    public void setDatasetHeader(Instances data) {
+        this.dataset = data;
+        this.dataset.setClassIndex(dataset.numAttributes() - 1);
+    }
+
+    // Classify a single instance
+    public String classifyEmail(double[] values) {
+        try {
+            // Create new instance with same number of attributes
+            Instance newInst = new weka.core.DenseInstance(dataset.numAttributes());
+            newInst.setDataset(dataset);
+
+            // Fill feature values (excluding class)
+            for (int i = 0; i < dataset.numAttributes() - 1; i++) {
+                newInst.setValue(i, values[i]);
+            }
+
+            // Predict
+            double prediction = model.classifyInstance(newInst);
+            String className = dataset.classAttribute().value((int) prediction);
+
+            return className;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
+        }
+    }
+
+    // Classify email from a CSV row (string of comma-separated values)
+    public String classifyEmailFromCSV(String csvRow) {
+        try {
+            String[] parts = csvRow.split(",");
+
+            // Expect dataset.numAttributes() - 1 values (exclude class label)
+            int expected = dataset.numAttributes() - 1;
+            if (parts.length != expected) {
+                return "Error: Expected " + expected + " values, but got " + parts.length;
+            }
+
+            double[] values = new double[expected];
+            for (int i = 0; i < expected; i++) {
+                values[i] = Double.parseDouble(parts[i]);
+            }
+
+            // Create instance
+            DenseInstance newInst = new DenseInstance(dataset.numAttributes());
+            newInst.setDataset(dataset);
+
+            for (int i = 0; i < expected; i++) {
+                newInst.setValue(i, values[i]);
+            }
+
+            // Predict
+            double prediction = model.classifyInstance(newInst);
+            return dataset.classAttribute().value((int) prediction);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
         }
     }
 
